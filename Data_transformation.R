8f7ed30b861d45da2db42a8b183784b39cfdb99c
kristianfoged <kristian.foged@gmail.com>
2021-12-13 22:12:12 +0100

    udpakning af raadata

--- Data_transformation.R
+++ Data_transformation.R
@@ -2,30 +2,26 @@
 
 
 ##install.packages("tidytext")
-##install.packages("googledrive")
+
 library(tidyverse)
 library(lubridate)
 library(tidytext)
-##library(googledrive)
 
-dagbog <- read.csv2("Dagbog_MP_csv.csv")
-dagbog2 <- read.csv2("mikkel_peter_almanak.csv")
-db <- dagbog %>% 
-  mutate(Dato = as_date(Dato_id)) 
+## Initial data
 
-ymd("19210912")
-db2 <- db %>%
-  mutate(date__year  = substr(Dato_id, 1, 4)) %>%
-  mutate(date__month = substr(Dato_id, 5, 2)) %>%
-  mutate(date__day   = substr(Dato_id, 7, 2)) 
+dagbog_raw <- read.csv2("mikkel_peters_dagbog_raw.csv")
+
+dagbog  <- dagbog_raw %>% mutate(YEAR_ = str_sub(Key,3,6)) %>% 
+                          mutate(MONTH_ = str_sub(Key,7,8)) %>%
+                          mutate(DAY_ = str_sub(Key,9,10)) %>%
+                          mutate(TYPE_ = str_sub(Key,11,12))
 
 dagbog <- as_tibble(dagbog)
-##adresser <- read_csv("adgangsadresser_samso")
+
 ##adresser <- as_tibble(adresser)
 ##
 ##dagbog_un <- dagbog %>%  unnest_tokens(word, Tekst_Mikkel_Peter)
 ##frekvens <- dagbog_un %>%  count(word, sort = TRUE) 
 ##
-##
-##frekvens_add <- adresser %>%  count(ejerlavnavn, sort = TRUE) 
+
 

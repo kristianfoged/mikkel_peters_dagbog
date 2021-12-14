47394286ed57b59aef04ee82e9fbf2ea0c5683f5
kristianfoged <kristian.foged@gmail.com>
2021-12-14 21:12:02 +0100

    v1

--- Data_transformation.R
+++ Data_transformation.R
@@ -2,28 +2,24 @@
 
 
 ##install.packages("tidytext")
-
+##install.packages("bookdown")
 library(tidyverse)
 library(lubridate)
 library(tidytext)
-
+library(bookdown)
 ## Initial data
 
 dagbog_raw <- read.csv2("mikkel_peters_dagbog_raw.csv")
 
 dagbog  <- dagbog_raw %>% 
-##  mutate(YEAR_ = str_sub(Key,3,6)) %>% 
-##  mutate(MONTH_ = str_sub(Key,7,8)) %>%
-##  mutate(DAY_ = str_sub(Key,9,10)) %>%
+ mutate(YEAR_ = str_sub(Key,3,6)) %>% 
+  mutate(MONTH_ = str_sub(Key,7,8)) %>%
+  mutate(DAY_ = str_sub(Key,9,10)) %>%
                           mutate(TYPE_ = str_sub(Key,11,12)) %>%
   mutate(DATO = str_c( str_sub(Key,3,6),str_sub(Key,7,8), str_sub(Key,9,10), sep = "-"))%>%
   mutate(DATO = as_date(DATO))
 
-
-
-str_c("Letter", letters, sep = ": ")
-dagbog <- as_tibble(dagbog)
-unite(dagbog, YEAR_,MONTH_ , sep = "-", remove = TRUE, na.rm = FALSE)
+saveRDS(dagbog, file = "mp_dagbog.rds")
 
 
 ##adresser <- as_tibble(adresser)
